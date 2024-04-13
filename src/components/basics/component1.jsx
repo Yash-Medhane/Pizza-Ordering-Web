abb3443259122ecbb603cb5c42c2f65b3338f10f
Yash-Medhane <yashmedhane43@gmail.com>
2024-04-14 01:15:36 +0530

    New Changes

--- src/components/basics/component1.jsx
+++ src/components/basics/component1.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import pizzaImage from '../assets/pizza20.jpg'; 
+import pizzaImage from '../assets/main-img.png'; 
+import Offer from "../assets/offer.png";
+import Delivery from "../assets/delivery.png"
+import Variety from "../assets/variety.png";
+import Ingrediants from "../assets/ingrediants.png"
 import "./Style/style.css"
 
 const component1 = () => {
@@ -7,11 +11,30 @@ const component1 = () => {
     <div>
       <div className="header">
       <div className="right">
-  <h1>Welcome to <span className="highlight">The PizzaHub</span>!</h1>
-  <p>Indulge in a culinary journey where every bite is a burst of flavor and satisfaction.</p>
-  <p>From classic favorites to unique creations, our pizzas are crafted with passion and expertise.</p>
-  <p>Explore our menu, tantalize your taste buds, and embark on a delicious adventure!</p>
-  <button className="explore-button">Explore Menu</button>
+  <div className="head">
+  <h1>Discover The PizzaHub</h1>
+  <blockquote className="quote">
+    "At The PizzaHub, we believe in crafting moments of joy, one slice at a time."
+  </blockquote>
+  </div>
+  <div className="highlights">
+    <div className="highlight">
+      <img src={Offer} alt="Special Offer" />
+      <p>Special Offer</p>
+    </div>
+    <div className="highlight">
+      <img src={Delivery} alt="Fast Delivery" />
+      <p>Fast Delivery</p>
+    </div>
+    <div className="highlight">
+  <img src={Variety} alt="Wide Variety" />
+  <p>Wide Variety</p>
+</div>
+<div className="highlight">
+  <img src={Ingrediants} alt="Quality Ingredients" />
+  <p>Quality Ingredients</p>
+</div>
+  </div>
 </div>
 
       <div className="left">
